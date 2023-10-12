8306c8b69f6931997aebda0ff096d68ce17766f4
muhmmad Nabh <muhammadnabih2020@gmail.com>
2023-10-12 12:56:38 +0300

    refactor code

--- src/components/MainContent.jsx
+++ src/components/MainContent.jsx
@@ -1,5 +1,4 @@
-/* eslint-disable react-hooks/exhaustive-deps */
-import  { useState, useEffect } from "react";
+import { useState, useEffect, useMemo } from "react";
 import Grid from "@mui/material/Grid";
 import Divider from "@mui/material/Divider";
 import Typography from "@mui/material/Typography";
@@ -11,14 +10,16 @@ const MainContent = () => {
   const { city, salahTimes } = useSalahContext();
   const { Fajr, Dhuhr, Asr, Maghrib, Isha } = salahTimes;
 
-  const prayerTimes = {
-    الفجر: parse(Fajr, "HH:mm", new Date()),
-    الظهر: parse(Dhuhr, "HH:mm", new Date()),
-    العصر: parse(Asr, "HH:mm", new Date()),
-    المغرب: parse(Maghrib, "HH:mm", new Date()),
-    العشاء: parse(Isha, "HH:mm", new Date()),
-  };
-
+  // استخدام useMemo لتحديد prayerTimes
+  const prayerTimes = useMemo(() => {
+    return {
+      الفجر: parse(Fajr, "HH:mm", new Date()),
+      الظهر: parse(Dhuhr, "HH:mm", new Date()),
+      العصر: parse(Asr, "HH:mm", new Date()),
+      المغرب: parse(Maghrib, "HH:mm", new Date()),
+      العشاء: parse(Isha, "HH:mm", new Date()),
+    };
+  }, [Fajr, Dhuhr, Asr, Maghrib, Isha]);
   const [currentPrayer, setCurrentPrayer] = useState("");
   const [timeRemaining, setTimeRemaining] = useState("");
 
@@ -41,8 +42,16 @@ const MainContent = () => {
       const secondsRemaining = differenceInSeconds(nextPrayerTime, new Date());
 
       if (secondsRemaining <= 0) {
-        clearInterval(timer);
         setTimeRemaining("0:00:00");
+        clearInterval(timer);
+
+        // إعادة تشغيل التايمر للبدء من جديد بعد انتهاء الوقت
+        const nextPrayer = Object.keys(prayerTimes).find(
+          (prayer) => now < prayerTimes[prayer]
+        );
+        nextPrayerTime = prayerTimes[nextPrayer];
+        setCurrentPrayer(nextPrayer);
+        setInterval(timer, 1000); // إعادة تشغيل التايمر
       } else {
         const hours = Math.floor(secondsRemaining / 3600);
         const minutes = Math.floor((secondsRemaining % 3600) / 60);
@@ -52,7 +61,7 @@ const MainContent = () => {
     }, 1000);
 
     return () => clearInterval(timer);
-  }, [prayerTimes]);
+  }, [prayerTimes, Fajr, Dhuhr, Asr, Maghrib, Isha]);
 
   const formattedDate = format(new Date(), "MMMM dd, yyyy | hh:mm a", {
     locale: ar,
@@ -62,24 +71,34 @@ const MainContent = () => {
     <nav>
       <Grid container spacing={2}>
         <Grid item xs={12} sm={6}>
-          <Typography sx={{
-  fontSize: {
-    xs: "1.2rem", // للشاشات الصغيرة
-    md: "1.8rem", // للشاشات الوسطى
-  },
-}} variant="h4">{formattedDate}</Typography>
+          <Typography
+            sx={{
+              fontSize: {
+                xs: "1.2rem", // للشاشات الصغيرة
+                md: "1.8rem", // للشاشات الوسطى
+              },
+            }}
+            variant="h4"
+          >
+            {formattedDate}
+          </Typography>
           <Typography variant="h5" color={"orange"}>
             {city}
           </Typography>
         </Grid>
 
         <Grid item xs={12} sm={6}>
-          <Typography  sx={{
-  fontSize: {
-    xs: "1.2rem", // للشاشات الصغيرة
-    md: "1.8rem", // للشاشات الوسطى
-  },
-}} variant="h4">متبقي حتى صلاة {currentPrayer}</Typography>
+          <Typography
+            sx={{
+              fontSize: {
+                xs: "1.2rem", // للشاشات الصغيرة
+                md: "1.8rem", // للشاشات الوسطى
+              },
+            }}
+            variant="h4"
+          >
+            متبقي حتى صلاة {currentPrayer}
+          </Typography>
           <Typography variant="h5" color={"orange"}>
             {timeRemaining}
           </Typography>
